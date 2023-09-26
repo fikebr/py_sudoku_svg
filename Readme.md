4cc17a3087c5a421c75386d3d17c491bf1e193fe
Brad Fike <brad@fikeweb.com>
2023-09-26 02:49:46 -0500

    added argparse

--- Readme.md
+++ Readme.md
@@ -4,6 +4,13 @@ Builds sudoku puzzle images in bulk
 
 ## Usage
 
+
+- Output (-o) Output filename. The name will be given a prefix of "001_"
+- Number (-n) The Number of files to create. Default 10
+- Size (-z) The size in pixels of the puzzle. Default 200
+- Difficulty (-d) The diffuculty of the puzzle. 1=easy, 2=med, 3=hard. Defualt 2
+- Template (-t) The png file to use as a template to place your puzzle in
+- Template Coordinates (-c) Where to place the puzzle in the template image. Format 10x10. Default 20x20
 ## Download and Install
 
 ## Todo
@@ -11,3 +18,5 @@ Builds sudoku puzzle images in bulk
 1) Add command-line processing
 2) Export to *.png files
 3) Template image: provide a template image and placement information and the puzzle will be placed in the template
+4) Compile to an exe
+
--- py_sudoku_svg.py
+++ py_sudoku_svg.py
@@ -1,11 +1,18 @@
 import drawsvg as draw
 from sudoku import Sudoku
+import argparse
+import os
 
 def main():
-    canvasSize = 300
-    d = draw.Drawing(canvasSize+4, canvasSize+4, origin=(0,0))
+    args = getArgs()
+    print('folder = {0}, file = {1}, cwd = {2}'.format(args.folder, args.file, os.path.split(__file__)[0]))
+
+    canvasSize = args.size
     cubeSize = canvasSize/9
-    puzzle = getPuzzle(0.5)
+
+    puzzle = getPuzzle(args.difficulty)
+
+    d = draw.Drawing(canvasSize, canvasSize, origin=(0,0))
     drawPuzzle(d, puzzle, cubeSize)
     draw3x3(d, cubeSize)
 
@@ -43,6 +50,35 @@ def drawCube(d, x=10, y=10, size=40, strNumber='0'):
     cx=x+(size/2)
     cy=y+int(size/10)+(size/2)
     d.append(draw.Text(str(strNumber), font_size=size-10, x=cx, y=cy, text_anchor='middle', dominant_baseline='middle'))
+
+def getArgs():
+
+    parser = argparse.ArgumentParser(
+                    prog='Sudoku Builder',
+                    description="Build Sudoku puzzle files in bulk.",
+                    epilog='Thank you.')
+
+    parser.add_argument('-o', '--output', type=str, help='Output filename. The name will be given a prefix of "001_"', required=True)
+    parser.add_argument('-n', '--number', type=int, help='The Number of files to create.', default=1)
+    parser.add_argument('-z', '--size', type=int, help='The size in pixels of the puzzle.', default=200)
+    parser.add_argument('-d', '--difficulty', type=int, help='The diffuculty of the puzzle. 1=easy, 2=med, 3=hard.', default=2, choices=[1, 2, 3])
+    parser.add_argument('-t', '--template', type=str, help='The png file to use as a template to place your puzzle in.')
+    parser.add_argument('-c', '--coords', type=str, help='Coordinates where to place the puzzle in the template image. Format 20x20.', default='20x20')
+
+    args = parser.parse_args()
+
+    DIFFICULTIES = [0, 0.3, 0.5, 0.8]
+    args.difficulty = DIFFICULTIES[args.difficulty]
+
+    (args.folder, args.file) = getOutput(args.output)
+
+    return(args)
+
+def getOutput(output):
+    (folder, file) = os.path.split(os.path.abspath(output))
+    # print('folder = {0}, file = {1}, cwd = {2}'.format(folder, file, os.path.split(__file__)[0]))
+    os.makedirs(folder)
+    return(folder, file)
 
 if __name__ == "__main__":
 	main()
